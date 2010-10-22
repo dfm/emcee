ae206749ceeb3999228c00e03be169cfdfc09d5b
Dan F-M <dan@danfm.ca>
2010-10-22 09:45:57 -0400

    LICENSE and README updates

--- test.py
+++ test.py
@@ -8,13 +8,12 @@
 
 
  Copyright 2010 Daniel Foreman-Mackey
- 
+  
  This is part of MarkovPy.
  
  MarkovPy is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+ it under the terms of the GNU General Public License version 2 as
+ published by the Free Software Foundation.
 
  MarkovPy is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
