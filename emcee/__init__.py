7d696dda45902ab5465d50ade619f3b7c67f7be8
Dan F-M <dan@danfm.ca>
2012-02-02 16:02:41 -0500

    a bunch of doc tweaks

--- emcee/__init__.py
+++ emcee/__init__.py
@@ -2,3 +2,26 @@ from sampler import *
 from mh import *
 from ensemble import *
 
+def test():
+    from tests import Tests
+
+    test_names = [("Metropolis-Hastings", "test_mh"),
+                  ("Ensemble Sampler",    "test_ensemble")]
+
+    print "Starting tests..."
+
+    failures = 0
+    tests = Tests()
+    for t in test_names:
+        tests.setUp()
+        try:
+            getattr(tests, t[1])()
+        except:
+            print "Test: %s failed."%(t[0])
+            failures += 1
+        else:
+            print "Test: %s passed."%(t[0])
+
+    print "%d tests passed"%(len(test_names)-failures)
+    print "%d tests failed"%(failures)
+
--- emcee/tests.py
+++ emcee/tests.py
@@ -26,7 +26,7 @@ class Tests:
         self.nwalkers = 100
         self.ndim     = 5
 
-        self.N = 3000
+        self.N = 1000
 
         self.mean = np.zeros(self.ndim)
         self.cov  = 0.5-np.random.rand(self.ndim*self.ndim).reshape((self.ndim,self.ndim))
@@ -50,7 +50,6 @@ class Tests:
         strt = time.time()
         for i in self.sampler.sample(p0, iterations=N):
             pass
-        print "Sampling took {:.3f} seconds".format(time.time() - strt)
 
         assert np.mean(self.sampler.acceptance_fraction) > 0.25
         chain = self.sampler.flatchain
@@ -66,36 +65,3 @@ class Tests:
         self.sampler = EnsembleSampler(self.nwalkers, self.ndim, lnprob_gaussian, args=[self.icov])
         self.check_sampler()
 
-if __name__ == '__main__':
-    import matplotlib.pyplot as pl
-    tests = Tests()
-    tests.setUp()
-
-    chains = []
-
-    for t in [tests.test_aimog, tests.test_mog, tests.test_dual2, tests.test_dual, tests.test_ensemble, tests.test_mh]:
-        np.random.seed(10)
-        tests.setUp()
-        print t
-        try:
-            t()
-            print "Mean autocorrelation time(s):\n\t",
-            print tests.sampler.acor
-        except Exception as e:
-            print e
-            print "failed!"
-        print "Mean acceptance fraction: {:.3f}".format(np.mean(tests.sampler.acceptance_fraction))
-        chains.append(tests.sampler.flatchain)
-
-    truth = tests.truth
-    for i in range(tests.ndim):
-        pl.figure(i)
-        pl.hist(truth[:,i],100,normed=True,histtype='stepfilled', color='k', alpha=0.4)
-
-    for chain in chains:
-        for i in range(tests.ndim):
-            pl.figure(i)
-            samps = chain[:,i].flatten()
-            pl.hist(samps,100,normed=True, histtype='step', lw=2)
-
-    pl.show()
