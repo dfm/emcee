874ed77a9075820d99bd48f7cb39c1fe3c3dd47e
Dan F-M <dan@danfm.ca>
2012-02-06 11:32:08 -0500

    adding quickstart code to document

--- quickstart.py
+++ quickstart.py
@@ -18,13 +18,20 @@ icov = np.linalg.inv(cov)
 nwalkers = 100
 p0 = [np.random.rand(ndim) for i in xrange(nwalkers)]
 
-sampler = emcee.EnsembleSampler(nwalkers, ndim,lnprob,
-                        postargs=[means, icov])
+sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=[means, icov])
 
-pos,prob,state = sampler.run_mcmc(p0, None, 500)
-sampler.clear_chain()
+pos, prob, state = sampler.run_mcmc(p0, 500)
+sampler.reset()
 
-sampler.run_mcmc(pos, state, 2000)
+sampler.run_mcmc(pos, 2000, rstate0=state)
 
 print "Mean acceptance fraction:", np.mean(sampler.acceptance_fraction)
+
+try:
+    import matplotlib.pyplot as pl
+except ImportError:
+    print "Try installing matplotlib to generate some sweet plots..."
+else:
+    pl.hist(sampler.flatchain[:,0], 100)
+    pl.show()
 
