0c4d448710cf205a95cdb02c3e00c25817906cd3
Dan F-M <danfm@nyu.edu>
2012-06-08 11:45:05 -0400

    pep8ify rosenbrock

--- examples/rosenbrock.py
+++ examples/rosenbrock.py
@@ -7,6 +7,7 @@ Sample code for sampling the Rosenbrock density using emcee.
 import numpy as np
 import emcee
 
+
 # Define the Density
 class Rosenbrock(object):
     def __init__(self):
@@ -14,18 +15,23 @@ class Rosenbrock(object):
         self.a2 = 20.0
 
     def __call__(self, p):
-        return -(self.a1 * (p[1]-p[0]**2)**2 + (1-p[0])**2)/self.a2
+        return -(self.a1 * (p[1] - p[0] ** 2) ** 2 + (1 - p[0]) ** 2) / self.a2
+
 
 nwalkers = 100
 
+
 # Make an initial guess for the positions. This is a pretty bad guess!
-p0 = np.random.rand(nwalkers*2).reshape(nwalkers,2)
+p0 = np.random.rand(nwalkers * 2).reshape(nwalkers, 2)
+
 
 # Instantiate the class
 rosenbrock = Rosenbrock()
 
+
 # The sampler object
 sampler = emcee.EnsembleSampler(nwalkers, 2, rosenbrock, threads=10)
+
 
 # Sample, outputting to a file
 f = open("rosenbrock.out", "w")
@@ -34,4 +40,3 @@ for pos, prob, rstate in sampler.sample(p0, iterations=2000):
     f.write("\n".join(["\t".join([str(q) for q in p]) for p in pos]))
     f.write("\n")
 f.close()
-
