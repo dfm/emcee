a413abf71e4a4348f9b9e97b15241a97082062b7
Will Meierjurgen Farr <wmfarr@gmail.com>
2012-10-15 17:31:28 +0100

    Better docs, and better .chain handling, allowing for skipping in ptsampler.py

--- emcee/ptsampler.py
+++ emcee/ptsampler.py
@@ -14,16 +14,30 @@ class PTPost(object):
     """Wrapper for posterior used in emcee."""
     
     def __init__(self, logl, logp, beta):
-        """Initialize with given log-likelihood, log-prior, and beta = 1/T."""
+        """:param logl: Function returning natural log of the
+            likelihood.
+
+        :param logp: Function returning natural log of the prior.
+
+        :param beta: Inverse temperature of this chain: lnpost =
+            beta*logl + logp"""
 
         self._logl = logl
         self._logp = logp
         self._beta = beta
 
     def __call__(self, x):
-        """Returns lnpost(x), lnlike(x) (the second value will be
-        treated as a blob by emcee), where lnpost(x) = beta*lnlike(x)
-        + lnprior(x)."""
+        """
+        :param x:
+        The position in parameter space.
+
+        Returns ``lnpost(x)``, ``lnlike(x)`` (the second value will be
+        treated as a blob by emcee), where 
+
+        .. math::
+
+            \ln \pi(x) \equiv \beta \ln l(x) + \ln p(x)
+        """
 
         lp = self._logp(x)
 
@@ -35,34 +49,48 @@ class PTPost(object):
 
         return self._beta*ll+lp, ll
 
-class PTSampler(object):
+class PTSampler(em.Sampler):
     """A parallel-tempered ensemble sampler, using ``EnsembleSampler``
     for sampling within each parallel chain.
 
-    :param ntemps: The number of temperatures.
+    :param ntemps: 
+        The number of temperatures.
 
-    :param nwalkers: The number of ensemble walkers at each
-        temperature.
+    :param nwalkers: 
+        The number of ensemble walkers at each temperature.
 
-    :param dim: The dimension of parameter space.
+    :param dim: 
+        The dimension of parameter space.
 
-    :param logl: The ln(likelihood) function.
+    :param logl: 
+        The ln(likelihood) function.
 
-    :param logp: The ln(prior) function.
+    :param logp: 
+        The ln(prior) function.
 
-    :param threads: The number of parallel threads to use in sampling.
+    :param threads: (optional)
+        The number of parallel threads to use in sampling.
 
-    :param pool: Alternative to ``threads``.  Any object that
-        implements a ``map`` method compatible with the built-in
-        ``map`` will do here.  For example, ``multiprocessing.Pool``.
+    :param pool: (optional) 
+        Alternative to ``threads``.  Any object that implements a
+        ``map`` method compatible with the built-in ``map`` will do
+        here.  For example, :class:`multi.Pool` will do.
 
-    :param betas: Array giving the inverse temperature ladder,
-    beta=1/T, used in the ladder.  The default is for an exponential
-    ladder, with beta decreasing by a factor of 1/sqrt(2) each rung."""
+    :param betas: (optional) 
+        Array giving the inverse temperatures, beta=1/T, used in the
+        ladder.  The default is for an exponential ladder, with beta
+        decreasing by a factor of 1/sqrt(2) each rung."""
     def __init__(self, ntemps, nwalkers, dim, logl, logp, threads=1, pool=None, betas=None):
+        self.logl=logl
+        self.logp=logp
+
         self.ntemps = ntemps
         self.nwalkers = nwalkers
         self.dim = dim
+
+        self._chain = None
+        self._lnprob = None
+        self._lnlikelihood = None
 
         if betas is None:
             self._betas = self.exponential_beta_ladder(ntemps)
@@ -79,7 +107,7 @@ class PTSampler(object):
         self.samplers = [em.EnsembleSampler(nwalkers, dim, PTPost(logl, logp, b), pool=self.pool) for b in self.betas]
 
     def exponential_beta_ladder(self, ntemps):
-        """Exponential ladder in 1/T, nwith T increasing by sqrt(2)
+        """Exponential ladder in 1/T, with T increasing by sqrt(2)
         each step, with ``ntemps`` in total."""
         return np.exp(np.linspace(0, -(ntemps-1)*0.5*np.log(2), ntemps))
 
@@ -91,68 +119,101 @@ class PTSampler(object):
         for s in self.samplers:
             s.reset()
 
-    def sample(self, p0, lnprob0=None, logl0=None, iterations=1, storechain=True):
-        """Advance the chains iterations steps as a generator.  
+        self.nswap = np.zeros(self.ntemps, dtype=np.float)
+        self.nswap_accepted = np.zeros(self.ntemps, dtype=np.float)
+
+        self._chain = None
+        self._lnprob = None
+        self._lnlikelihood = None
+
+    def sample(self, p0, lnprob0=None, logl0=None, iterations=1, thin=1, storechain=True):
+        """Advance the chains ``iterations`` steps as a generator.
         
-        :param p0: The initial positions of the walkers.  Shape should
-        be ``(ntemps, nwalkers, dim)``.
+        :param p0: 
+            The initial positions of the walkers.  Shape should be
+            ``(ntemps, nwalkers, dim)``.
 
-        :param lnprob0: The initial ln(posterior) values for the
-        ensembles.  Shape ``(ntemps, nwalkers)``.
+        :param lnprob0: (optional) 
+            The initial posterior values for the ensembles.  Shape
+            ``(ntemps, nwalkers)``.
 
-        :param logl0: The initial ln(likelihood) values for the
-        ensembles.  Shape ``(ntemps, nwalkers)``.
+        :param logl0: (optional) 
+            The initial likelihood values for the ensembles.  Shape
+            ``(ntemps, nwalkers)``.
 
-        :param iterations: The number of iterations to preform.
+        :param iterations: (optional)
+            The number of iterations to preform.
 
-        :param storechain: If ``True`` store the iterations in the
-        ``chain`` property.
+        :param thin: (optional)
+            The number of iterations to perform between saving the
+            state to the internal chain.
+
+        :param storechain: (optional) 
+            If ``True`` store the iterations in the ``chain``
+            property.
 
         At each iteration, this generator yields
 
         * ``p``, the current position of the walkers.
 
-        * ``lnprob`` the current ln(posterior) values for the walkers.
+        * ``lnprob`` the current posterior values for the walkers.
 
-        * ``lnlike`` the current ln(likelihood) values for the walkers."""
+        * ``lnlike`` the current likelihood values for the walkers."""
 
         p = p0
 
-        # If we have no lnprob or blobs, then run at least one
-        # iteration to compute them.
+        # If we have no lnprob or logls compute them
         if lnprob0 is None or logl0 is None:
-            iterations -= 1
-            
-            lnprob = []
-            logl = []
-            for i,s in enumerate(self.samplers):
-                for psamp, lnprobsamp, rstatesamp, loglsamp in s.sample(p[i,...], storechain=storechain):
-                    p[i,...] = psamp
-                    lnprob.append(lnprobsamp)
-                    logl.append(loglsamp)
+            lnprob0=np.zeros((self.ntemps, self.nwalkers))
+            logl0=np.zeros((self.ntemps, self.nwalkers))
+            for i in range(self.ntemps):
+                fn=PTPost(self.logl, self.logp, self.betas[i])
+                if self.pool is None:
+                    results=map(fn, p[i,:,:])
+                else:
+                    results=self.pool.map(fn, p[i,:,:])
 
-            lnprob = np.array(lnprob) # Dimensions (ntemps, nwalkers)
-            logl = np.array(logl)
+                lnprob0[i,:] = np.array([r[0] for r in results])
+                logl0[i,:] = np.array([r[1] for r in results])
 
-            p,lnprob,logl = self.temperature_swaps(p, lnprob, logl)
-        else:
-            lnprob = lnprob0
-            logl = logl0
+        lnprob = lnprob0
+        logl = logl0
+
+        if storechain:
+            nsave=iterations/thin
+            if self._chain is None:
+                isave=0
+                self._chain = np.zeros((self.ntemps, self.nwalkers, nsave, self.dim))
+                self._lnprob = np.zeros((self.ntemps, self.nwalkers, nsave))
+                self._lnlikelihood = np.zeros((self.ntemps, self.nwalkers, nsave))
+            else:
+                isave=self._chain.shape[2]
+                self._chain = np.concatenate((self._chain, np.zeros((self.ntemps, self.nwalkers, nsave, self.dim))), axis=2)
+                self._lnprob = np.concatenate((self._lnprob, np.zeros((self.ntemps, self.nwalkers, nsave))), axis=2)
+                self._lnlikelihood = np.concatenate((self._lnlikelihood,
+                                                     np.zeros((self.ntemps, self.nwalkers, nsave))), axis=2)
 
         for i in range(iterations):
-            for i,s in enumerate(self.samplers):
-                for psamp, lnprobsamp, rstatesamp, loglsamp in s.sample(p[i,...], lnprob0=lnprob[i,...], blobs0=logl[i,...], storechain=storechain):
-                    p[i,...] = psamp
-                    lnprob[i,...] = lnprobsamp
-                    logl[i,...] = np.array(loglsamp)
+            for j,s in enumerate(self.samplers):
+                for psamp, lnprobsamp, rstatesamp, loglsamp in s.sample(p[j,...], lnprob0=lnprob[j,...], blobs0=logl[j,...], storechain=False):
+                    p[j,...] = psamp
+                    lnprob[j,...] = lnprobsamp
+                    logl[j,...] = np.array(loglsamp)
 
-            p,lnprob,logl = self.temperature_swaps(p, lnprob, logl)
+            p,lnprob,logl = self._temperature_swaps(p, lnprob, logl)
+
+            if (i+1)%thin == 0:
+                if storechain:
+                    self._chain[:,:,isave,:] = p
+                    self._lnprob[:,:,isave,] = lnprob
+                    self._lnlikelihood[:,:,isave] = logl
+                    isave += 1
 
             yield p, lnprob, logl
 
-    def temperature_swaps(self, p, lnprob, logl):
+    def _temperature_swaps(self, p, lnprob, logl):
         """Perform parallel-tempering temperature swaps on the state
-        in p with associated lnprob and logl."""
+        in ``p`` with associated ``lnprob`` and ``logl``."""
 
         ntemps=self.ntemps
 
@@ -191,42 +252,78 @@ class PTSampler(object):
 
     @property
     def betas(self):
-        """Returns the sequence of inverse temperatures in the
-        ladder."""
+        """Returns the sequence of inverse temperatures in the ladder."""
         return self._betas
 
     @property 
     def chain(self):
-        """Returns the stored chain of samples.  Will have shape that
-        is ``(Ntemps, Nwalkers, Nsteps, Ndim)``."""
+        """Returns the stored chain of samples; shape ``(Ntemps,
+        Nwalkers, Nsteps, Ndim)``."""
 
-        return np.array([s.chain for s in self.samplers])
+        return self._chain
 
     @property
     def lnprobability(self):
-        """Matrix of lnprobability values, of shape ``(Ntemps, Nwalkers, Nsteps)``"""
-        return np.array([s.lnprobability for s in self.samplers])
+        """Matrix of lnprobability values; shape ``(Ntemps, Nwalkers,
+        Nsteps)``"""
+        return self._lnprob
 
     @property
     def lnlikelihood(self):
-        """Matrix of ln-likelihood values of shape ``(Ntemps, Nwalkers, Nsteps)``."""
-        return np.array([np.transpose(np.array(s.blobs)) for s in self.samplers])
+        """Matrix of ln-likelihood values; shape ``(Ntemps, Nwalkers,
+        Nsteps)``."""
+        return self._lnlikelihood
 
     @property
     def tswap_acceptance_fraction(self):
         """Returns an array of accepted temperature swap fractions for
-        each temperature, shape ``(ntemps, )``."""
+        each temperature; shape ``(ntemps, )``."""
         return self.nswap_accepted / self.nswap
 
     @property
     def acceptance_fraction(self):
-        """Matrix of shape ``(Ntemps, Nwalkers)`` detailing the acceptance
-        fraction for each walker."""
+        """Matrix of shape ``(Ntemps, Nwalkers)`` detailing the
+        acceptance fraction for each walker."""
         return np.array([s.acceptance_fraction for s in self.samplers])
 
     @property
     def acor(self):
         """Returns a matrix of autocorrelation lengths for each
         parameter in each temperature of shape ``(Ntemps, Ndim)``."""
-        return np.array([s.acor for s in self.samplers])
+        
+        if acor is None:
+            raise ImportError('acor')
+        else:
+            acors=np.zeros((self.ntemps, self.dim))
 
+            for i in range(self.ntemps):
+                for j in range(self.dim):
+                    acors[i,j] = acor.acor(self._chain[i, :, :, j])[0]
+
+            return acors
+
+    def thermodynamic_integration_log_evidence(self, fburnin=0.5):
+        """Thermodynamic integration estimate of the evidence.
+
+        :param fburnin: (optional) The fraction of the chain to
+          discard as burnin samples; only the final ``1-fburnin``
+          fraction of the samples will be used to compute the
+          evidence.
+
+        The evidence is the integral of the un-normalized posterior
+        over all of parameter space:
+
+        .. math:: 
+
+            Z \\equiv \\int d\\theta \\, l(\\theta) p(\\theta)
+
+        Thermodymanic integration is a technique for estimating the
+        evidence integral using information from the chains at various
+        temperatures."""
+
+        betas=np.concatenate(self.betas, np.array([0]))
+        logls=self.lnlikelihood
+
+        istart=int(logls.shape[2]*fburnin) + 1
+
+        mean_logls=np.mean(np.mean(logls, axis=1)[:, istart:], axis=1)
