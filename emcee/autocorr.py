3bf5c94dedcd246a11c681d10994ea6de471fef2
Dan F-M <danfm@nyu.edu>
2014-01-10 15:14:35 -0500

    autocorr dealing with multidimensional arrays properly

--- emcee/autocorr.py
+++ emcee/autocorr.py
@@ -44,13 +44,27 @@ def integrated_time(x, axis=0):
     # Compute the autocorrelation function.
     f = function(x, axis=axis)
 
-    # Compute the integrated autocorrelation time.
-    first = [slice(None), ] * len(f.shape)
-    first[axis] = 0
-    sig2 = f[first]
-    N = f.shape[axis]
-    tau = 1 + 2*np.sum((1-np.arange(1, len(f))/N)*f[1:]/sig2, axis=axis)
+    # Special case 1D for simplicity.
+    if len(f.shape) == 1:
+        N = len(f)
+        return 1 + 2*np.sum((1-np.arange(1, N)/N)*f[1:]/f[0])
 
-    # if full_output:
-    #     return tau, np.mean(x), np.sqrt(sig2*tau/N)
+    # Estimate the variance along the time axis.
+    m = [slice(None), ] * len(f.shape)
+    m[axis] = 0
+    sig2 = f[m]
+
+    # Compute the broadcasting mask for the time array.
+    bc = [np.newaxis, ] * len(f.shape)
+    bc[axis] = slice(None)
+
+    # Compute the broadcasting mask for the variance array.
+    var_bc = [slice(None), ] * len(f.shape)
+    var_bc[axis] = np.newaxis
+
+    # Compute the integrated autocorrelation time.
+    N = f.shape[axis]
+    m[axis] = slice(1, N)
+    tau = 1 + 2*np.sum((1-np.arange(1, N)/N)[bc]*f[m]/sig2[var_bc], axis=axis)
+
     return tau
