0a133d6da181dedc38e77b0aa94dc9fe7c862ac7
Gregory Ashton <gash789@gmail.com>
2015-08-19 13:24:06 +0100

    Fixes axes labels and transformation of error

--- examples/pt_thermodynamic_integration.py
+++ examples/pt_thermodynamic_integration.py
@@ -25,14 +25,14 @@ out = sampler.run_mcmc(p0, 200)
 
 ln_evidence, ln_error = sampler.thermodynamic_integration_log_evidence()
 evidence = np.exp(ln_evidence)
-error = abs(ln_evidence) * ln_error
+error = np.exp(ln_evidence) * ln_error
 print "Evidence={} +/- {}".format(evidence, error)
 
 # Plot of the <ln(Z)> against beta which is integrated over
 logls = sampler.lnlikelihood
 mean_logls = np.mean(np.mean(logls, axis=1)[:, :], axis=1)
 plt.semilogx(betas, mean_logls, "-")
-plt.ylabel(r"Mean likelihood")
+plt.ylabel(r"Mean log-likelihood")
 plt.xlabel(r"$\beta = \frac{1}{T}$")
 plt.tight_layout()
 plt.savefig("betas.png")
