a36bfaca62de329cd196e8b5e1f171631d63c602
Dan F-M <danfm@nyu.edu>
2017-10-05 17:32:15 -0400

    travis install

---
+++ .ci/travis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+# http://conda.pydata.org/docs/travis.html#the-travis-yml-file
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
+else
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+fi
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+
+# Conda Python
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda info -a
+conda create --yes -n test python=$PYTHON_VERSION
+source activate test
+conda install numpy=$NUMPY_VERSION scipy setuptools pytest pytest-cov pip
+pip install coveralls
+
+# Build the extension
+python setup.py develop
