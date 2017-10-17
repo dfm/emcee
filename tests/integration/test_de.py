5ac39b4cbb4c670220b57c0a24f1798da16b674b
Dan F-M <danfm@nyu.edu>
2017-10-17 11:16:55 -0400

    no gamma test for de

--- tests/integration/test_de.py
+++ tests/integration/test_de.py
@@ -5,11 +5,15 @@ from __future__ import division, print_function
 from emcee import moves
 from .test_proposal import _test_normal, _test_uniform
 
-__all__ = ["test_normal_de", "test_uniform_de"]
+__all__ = ["test_normal_de", "test_normal_de_no_gamma", "test_uniform_de"]
 
 
 def test_normal_de(**kwargs):
     _test_normal(moves.DEMove(1e-2), **kwargs)
+
+
+def test_normal_de_no_gamma(**kwargs):
+    _test_normal(moves.DEMove(1e-2, 1e-2), **kwargs)
 
 
 def test_uniform_de(**kwargs):
