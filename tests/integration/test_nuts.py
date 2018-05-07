616d8db4c099336191998ca85766c914260857f2
Dan F-M <danfm@nyu.edu>
2018-05-07 13:24:36 -0400

    a few more nuts tests

--- tests/integration/test_nuts.py
+++ tests/integration/test_nuts.py
@@ -4,18 +4,34 @@ from __future__ import division, print_function
 
 from multiprocessing import Pool
 import pytest
+import numpy as np
+
 from emcee import moves
+from emcee.moves.nuts import (
+    IdentityMetric, IsotropicMetric, DiagonalMetric, DenseMetric
+)
+
 from .test_proposal import _test_normal
 
 __all__ = ["test_normal_nuts"]
 
 
+@pytest.mark.parametrize("metric", [None, IdentityMetric(3),
+                                    IsotropicMetric(3),
+                                    DiagonalMetric(np.ones(3)),
+                                    DenseMetric(np.eye(3))])
 @pytest.mark.parametrize("pool", [True, False])
-def test_normal_nuts(pool, **kwargs):
+@pytest.mark.parametrize("tune", [True, False])
+def test_normal_nuts(pool, metric, tune, **kwargs):
     if pool:
         kwargs["pool"] = Pool()
+    if tune:
+        move = moves.NUTSMove(ntune=300)
+    else:
+        move = moves.NUTSMove()
+    kwargs["ndim"] = 3
     kwargs["check_acceptance"] = False
     kwargs["nsteps"] = 100
-    _test_normal(moves.NUTSMove(), **kwargs)
+    _test_normal(move, **kwargs)
     if pool:
         kwargs["pool"].close()
