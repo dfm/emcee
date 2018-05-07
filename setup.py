202f9a442d7f11b598b03e7d30cb8ea237c22c62
Dan F-M <danfm@nyu.edu>
2018-05-07 16:26:05 -0400

    bug in setup

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     version=emcee.__version__,
     author="Daniel Foreman-Mackey",
     author_email="foreman.mackey@gmail.com",
-    packages=["emcee", "emcee.moves", "emcee.moves.nuts", "emcee.backends"],
+    packages=["emcee", "emcee.moves", "emcee.moves.hmc", "emcee.backends"],
     url="http://emcee.readthedocs.io",
     license="MIT",
     description=("The Python ensemble sampling toolkit for affine-invariant "
