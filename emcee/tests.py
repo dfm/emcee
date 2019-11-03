e38081e3ca312ee8fd7546438d42919c81a5082d
Dan F-M <foreman.mackey@gmail.com>
2019-11-03 07:50:38 -0500

    fixing nan test

--- emcee/tests.py
+++ emcee/tests.py
@@ -32,8 +32,11 @@ def log_unit_sphere_volume(ndim):
         logfactorial = 0.0
         for i in range(1, ndim + 1, 2):
             logfactorial += np.log(i)
-        return (ndim + 1) / 2.0 * np.log(2.0) \
-            + (ndim - 1) / 2.0 * np.log(np.pi) - logfactorial
+        return (
+            (ndim + 1) / 2.0 * np.log(2.0)
+            + (ndim - 1) / 2.0 * np.log(np.pi)
+            - logfactorial
+        )
 
 
 class LogLikeGaussian(object):
@@ -63,7 +66,7 @@ class LogPriorGaussian(object):
 
         if self.cutoff is not None:
             if -dist2 > self.cutoff * self.cutoff / 2.0:
-                return float('-inf')
+                return float("-inf")
             else:
                 return 0.0
         else:
@@ -75,7 +78,6 @@ def ln_flat(x):
 
 
 class Tests:
-
     def setUp(self):
         np.random.seed(42)
 
@@ -87,14 +89,16 @@ class Tests:
         self.N = 3000
 
         self.mean = np.zeros(self.ndim)
-        self.cov = 0.5 - np.random.rand(self.ndim ** 2) \
-            .reshape((self.ndim, self.ndim))
+        self.cov = 0.5 - np.random.rand(self.ndim ** 2).reshape(
+            (self.ndim, self.ndim)
+        )
         self.cov = np.triu(self.cov)
         self.cov += self.cov.T - np.diag(self.cov.diagonal())
         self.cov = np.dot(self.cov, self.cov)
         self.icov = np.linalg.inv(self.cov)
-        self.p0 = [0.1 * np.random.randn(self.ndim)
-                   for i in range(self.nwalkers)]
+        self.p0 = [
+            0.1 * np.random.randn(self.ndim) for i in range(self.nwalkers)
+        ]
         self.truth = np.random.multivariate_normal(self.mean, self.cov, 100000)
 
     def check_sampler(self, N=None, p0=None):
@@ -110,13 +114,15 @@ class Tests:
         assert np.all(self.sampler.acceptance_fraction > 0)
 
         chain = self.sampler.flatchain
-        maxdiff = 10. ** (logprecision)
-        assert np.all((np.mean(chain, axis=0) - self.mean) ** 2 / self.N ** 2
-                      < maxdiff)
-        assert np.all((np.cov(chain, rowvar=0) - self.cov) ** 2 / self.N ** 2
-                      < maxdiff)
+        maxdiff = 10.0 ** (logprecision)
+        assert np.all(
+            (np.mean(chain, axis=0) - self.mean) ** 2 / self.N ** 2 < maxdiff
+        )
+        assert np.all(
+            (np.cov(chain, rowvar=0) - self.cov) ** 2 / self.N ** 2 < maxdiff
+        )
         tau = self.sampler.get_autocorr_time()
-        assert tau.shape == (self.ndim, )
+        assert tau.shape == (self.ndim,)
 
     def check_pt_sampler(self, cutoff, N=None, p0=None):
         if N is None:
@@ -128,52 +134,63 @@ class Tests:
             pass
 
         # Weaker assertions on acceptance fraction
-        assert np.mean(self.sampler.acceptance_fraction) > 0.1, \
-            "acceptance fraction < 0.1"
-        assert np.mean(self.sampler.tswap_acceptance_fraction) > 0.1, \
-            "tswap acceptance frac < 0.1"
+        assert (
+            np.mean(self.sampler.acceptance_fraction) > 0.1
+        ), "acceptance fraction < 0.1"
+        assert (
+            np.mean(self.sampler.tswap_acceptance_fraction) > 0.1
+        ), "tswap acceptance frac < 0.1"
 
         maxdiff = 10.0 ** logprecision
 
-        chain = np.reshape(self.sampler.chain[0, ...],
-                           (-1, self.sampler.chain.shape[-1]))
+        chain = np.reshape(
+            self.sampler.chain[0, ...], (-1, self.sampler.chain.shape[-1])
+        )
 
         # np.savetxt('/tmp/chain.dat', chain)
 
-        log_volume = self.ndim * np.log(cutoff) \
-            + log_unit_sphere_volume(self.ndim) \
+        log_volume = (
+            self.ndim * np.log(cutoff)
+            + log_unit_sphere_volume(self.ndim)
             + 0.5 * np.log(np.linalg.det(self.cov))
-        gaussian_integral = self.ndim / 2.0 * np.log(2.0 * np.pi) \
-            + 0.5 * np.log(np.linalg.det(self.cov))
+        )
+        gaussian_integral = self.ndim / 2.0 * np.log(
+            2.0 * np.pi
+        ) + 0.5 * np.log(np.linalg.det(self.cov))
 
         lnZ, dlnZ = self.sampler.thermodynamic_integration_log_evidence()
 
-        assert np.abs(lnZ - (gaussian_integral - log_volume)) < 3 * dlnZ, \
-            ("evidence incorrect: {0:g} versus correct {1:g} (uncertainty "
-             "{2:g})").format(lnZ, gaussian_integral - log_volume, dlnZ)
-        assert np.all((np.mean(chain, axis=0) - self.mean) ** 2.0 / N ** 2.0
-                      < maxdiff), 'mean incorrect'
-        assert np.all((np.cov(chain, rowvar=0) - self.cov) ** 2.0 / N ** 2.0
-                      < maxdiff), 'covariance incorrect'
+        assert np.abs(lnZ - (gaussian_integral - log_volume)) < 3 * dlnZ, (
+            "evidence incorrect: {0:g} versus correct {1:g} (uncertainty "
+            "{2:g})"
+        ).format(lnZ, gaussian_integral - log_volume, dlnZ)
+        assert np.all(
+            (np.mean(chain, axis=0) - self.mean) ** 2.0 / N ** 2.0 < maxdiff
+        ), "mean incorrect"
+        assert np.all(
+            (np.cov(chain, rowvar=0) - self.cov) ** 2.0 / N ** 2.0 < maxdiff
+        ), "covariance incorrect"
 
     def test_mh(self):
-        self.sampler = MHSampler(self.cov, self.ndim, lnprob_gaussian,
-                                 args=[self.icov])
+        self.sampler = MHSampler(
+            self.cov, self.ndim, lnprob_gaussian, args=[self.icov]
+        )
         self.check_sampler(N=self.N * self.nwalkers, p0=self.p0[0])
 
     def test_ensemble(self):
-        self.sampler = EnsembleSampler(self.nwalkers, self.ndim,
-                                       lnprob_gaussian, args=[self.icov])
+        self.sampler = EnsembleSampler(
+            self.nwalkers, self.ndim, lnprob_gaussian, args=[self.icov]
+        )
         self.check_sampler()
 
     def test_nan_lnprob(self):
-        self.sampler = EnsembleSampler(self.nwalkers, self.ndim,
-                                       lnprob_gaussian_nan,
-                                       args=[self.icov])
+        self.sampler = EnsembleSampler(
+            self.nwalkers, self.ndim, lnprob_gaussian_nan, args=[self.icov]
+        )
 
         # If a walker is right at zero, ``lnprobfn`` returns ``np.nan``.
         p0 = self.p0
-        p0[0] = 0.0
+        p0[0][:] = 0.0
 
         try:
             self.check_sampler(p0=p0)
@@ -184,8 +201,9 @@ class Tests:
         assert False, "We should never get here."
 
     def test_inf_nan_params(self):
-        self.sampler = EnsembleSampler(self.nwalkers, self.ndim,
-                                       lnprob_gaussian, args=[self.icov])
+        self.sampler = EnsembleSampler(
+            self.nwalkers, self.ndim, lnprob_gaussian, args=[self.icov]
+        )
 
         # Set one of the walkers to have a ``np.nan`` value.
         p0 = self.p0
@@ -228,18 +246,27 @@ class Tests:
             assert False, "The sampler should have failed by now."
 
     def test_parallel(self):
-        self.sampler = EnsembleSampler(self.nwalkers, self.ndim,
-                                       lnprob_gaussian, args=[self.icov],
-                                       threads=2)
+        self.sampler = EnsembleSampler(
+            self.nwalkers,
+            self.ndim,
+            lnprob_gaussian,
+            args=[self.icov],
+            threads=2,
+        )
         self.check_sampler()
 
     def test_pt_sampler(self):
         cutoff = 10.0
-        self.sampler = PTSampler(self.ntemp, self.nwalkers, self.ndim,
-                                 LogLikeGaussian(self.icov),
-                                 LogPriorGaussian(self.icov, cutoff=cutoff))
-        p0 = np.random.multivariate_normal(mean=self.mean, cov=self.cov,
-                                           size=(self.ntemp, self.nwalkers))
+        self.sampler = PTSampler(
+            self.ntemp,
+            self.nwalkers,
+            self.ndim,
+            LogLikeGaussian(self.icov),
+            LogPriorGaussian(self.icov, cutoff=cutoff),
+        )
+        p0 = np.random.multivariate_normal(
+            mean=self.mean, cov=self.cov, size=(self.ntemp, self.nwalkers)
+        )
         self.check_pt_sampler(cutoff, p0=p0, N=1000)
 
     def test_blobs(self):
@@ -248,10 +275,11 @@ class Tests:
         self.check_sampler()
 
         # Make sure that the shapes of everything are as expected.
-        assert (self.sampler.chain.shape == (self.nwalkers, self.N, self.ndim)
-                and len(self.sampler.blobs) == self.N
-                and len(self.sampler.blobs[0]) == self.nwalkers), \
-            "The blob dimensions are wrong."
+        assert (
+            self.sampler.chain.shape == (self.nwalkers, self.N, self.ndim)
+            and len(self.sampler.blobs) == self.N
+            and len(self.sampler.blobs[0]) == self.nwalkers
+        ), "The blob dimensions are wrong."
 
         # Make sure that the blobs aren't all the same.
         blobs = self.sampler.blobs
@@ -259,8 +287,9 @@ class Tests:
 
     def test_run_mcmc_resume(self):
 
-        self.sampler = s = EnsembleSampler(self.nwalkers, self.ndim,
-                                           lnprob_gaussian, args=[self.icov])
+        self.sampler = s = EnsembleSampler(
+            self.nwalkers, self.ndim, lnprob_gaussian, args=[self.icov]
+        )
 
         # first time around need to specify p0
         try:
@@ -278,8 +307,9 @@ class Tests:
         assert s.chain.shape[1] == 2 * self.N
 
     def test_deterministic(self):
-        self.sampler = s = EnsembleSampler(self.nwalkers, self.ndim,
-                                           lnprob_gaussian, args=[self.icov])
+        self.sampler = s = EnsembleSampler(
+            self.nwalkers, self.ndim, lnprob_gaussian, args=[self.icov]
+        )
 
         # Normal usage
         rstate0 = s.random_state
@@ -291,5 +321,7 @@ class Tests:
         rstate_obj = np.random.mtrand.RandomState()
         rstate_obj.set_state(rstate0)
         p3, lnprob3 = s.run_mcmc(self.p0, self.N, rstate0=rstate_obj)[:2]
-        assert np.all(p1 == p3) and np.all(lnprob1 == lnprob3), \
-           "Failed to set state from np.mtrand.RandomState object"
+        assert np.all(p1 == p3) and np.all(
+            lnprob1 == lnprob3
+        ), "Failed to set state from np.mtrand.RandomState object"
+
