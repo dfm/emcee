bee076fbf7c381c0edbcfafb7b29e5d3f0a95b89
Dan Foreman-Mackey <foreman.mackey@gmail.com>
2022-05-02 14:45:45 -0400

    Make the sample state indexable (#425)
    * fixing #424
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fixing myst issues in the docs
    
    * docs execute argument name
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -48,5 +48,5 @@ html_theme_options = {
     "use_repository_button": True,
     "use_download_button": True,
 }
-jupyter_execute_notebooks = "off"
-execution_timeout = -1
+nb_execution_mode = "off"
+nb_execution_timeout = -1
--- src/emcee/tests/unit/test_state.py
+++ src/emcee/tests/unit/test_state.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import pytest
 
 from emcee import EnsembleSampler
 from emcee.state import State
+
+
+def check_rstate(a, b):
+    assert all(np.allclose(a_, b_) for a_, b_ in zip(a[1:], b[1:]))
 
 
 def test_back_compat(seed=1234):
@@ -18,13 +23,13 @@ def test_back_compat(seed=1234):
     assert np.allclose(coords, c)
     assert np.allclose(log_prob, l)
     assert np.allclose(blobs, b)
-    assert all(np.allclose(a, b) for a, b in zip(rstate[1:], r[1:]))
+    check_rstate(rstate, r)
 
     state = State(coords, log_prob, None, rstate)
     c, l, r = state
     assert np.allclose(coords, c)
     assert np.allclose(log_prob, l)
-    assert all(np.allclose(a, b) for a, b in zip(rstate[1:], r[1:]))
+    check_rstate(rstate, r)
 
 
 def test_overwrite(seed=1234):
@@ -40,3 +45,28 @@ def test_overwrite(seed=1234):
     sampler = EnsembleSampler(nwalkers, 1, ll)
     sampler.run_mcmc(p0, 10)
     assert np.allclose(init, p0)
+
+
+def test_indexing(seed=1234):
+    np.random.seed(seed)
+    coords = np.random.randn(16, 3)
+    log_prob = np.random.randn(len(coords))
+    blobs = np.random.randn(len(coords))
+    rstate = np.random.get_state()
+
+    state = State(coords, log_prob, blobs, rstate)
+    np.testing.assert_allclose(state[0], state.coords)
+    np.testing.assert_allclose(state[1], state.log_prob)
+    check_rstate(state[2], state.random_state)
+    np.testing.assert_allclose(state[3], state.blobs)
+    np.testing.assert_allclose(state[-1], state.blobs)
+    with pytest.raises(IndexError):
+        state[4]
+
+    state = State(coords, log_prob, random_state=rstate)
+    np.testing.assert_allclose(state[0], state.coords)
+    np.testing.assert_allclose(state[1], state.log_prob)
+    check_rstate(state[2], state.random_state)
+    check_rstate(state[-1], state.random_state)
+    with pytest.raises(IndexError):
+        state[3]
