b9e0b70b0b5161a6cd31ac81181c1c38f95b076e
Gregor Boehl <admin@gregorboehl.com>
2022-10-10 11:35:35 +0200

    skip test if no scipy

--- src/emcee/tests/integration/test_dime.py
+++ src/emcee/tests/integration/test_dime.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+try:
+    import scipy
+except ImportError:
+    scipy = None
+import pytest
+
 from emcee import moves
 
 from .test_proposal import _test_normal, _test_uniform
@@ -7,8 +13,10 @@ from .test_proposal import _test_normal, _test_uniform
 __all__ = ["test_normal_dime", "test_uniform_de"]
 
 
+@pytest.mark.skipif(scipy is None, reason="scipy is not available")
 def test_normal_dime(**kwargs):
     _test_normal(moves.DIMEMove(), **kwargs)
 
+@pytest.mark.skipif(scipy is None, reason="scipy is not available")
 def test_uniform_dime(**kwargs):
     _test_uniform(moves.DIMEMove(), **kwargs)
