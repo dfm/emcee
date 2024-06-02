6399a33582d0d021d988a2773bf1315d478c6580
Gregor Boehl <admin@gregorboehl.com>
2024-06-02 07:39:48 +0200

    update & sync

--- src/emcee/moves/dime.py
+++ src/emcee/moves/dime.py
@@ -16,7 +16,7 @@ __all__ = ["DIMEMove"]
 def mvt_sample(df, mean, cov, size, random):
     """Sample from multivariate t distribution
 
-    For reasons beyond my understanding, the results from random.multivariate_normal with non-identity covariance matrix are not reproducibel across architecture. Since scipy.stats.multivariate_t is based on numpy's multivariate_normal, the workaround is to crochet this manually. Advantage is that the scipy dependency drops out.
+    The results from random.multivariate_normal with non-identity covariance matrix are not reproducibel across OS architecture. Since scipy.stats.multivariate_t is based on numpy's multivariate_normal, the workaround is to provide this manually.
     """
 
     dim = len(mean)
@@ -37,20 +37,22 @@ class DIMEMove(RedBlueMove):
 
     This is the `Differential-Independence Mixture Ensemble proposal` as developed in `Ensemble MCMC Sampling for DSGE Models <https://gregorboehl.com/live/dime_mcmc_boehl.pdf>`_.
 
-    Args:
-        sigma (Optional[float]): standard deviation of the Gaussian used to stretch the proposal vector.
-        gamma (Optional[float]): mean stretch factor for the proposal vector. By default, it is :math:`2.38 / \sqrt{2\,\mathrm{ndim}}` as recommended by `ter Braak (2006) <http://www.stat.columbia.edu/~gelman/stuff_for_blog/cajo.pdf>`_.
-        aimh_prob (Optional[float]): probability to draw an adaptive independence Metropolis Hastings (AIMH) proposal. By default this is set to :math:`0.1`.
-        df_proposal_dist (Optional[float]): degrees of freedom of the multivariate t distribution used for AIMH proposals. Defaults to :math:`10`.
+    Parameters
+    ----------
+    sigma : float, optional
+        standard deviation of the Gaussian used to stretch the proposal vector.
+    gamma : float, optional
+        mean stretch factor for the proposal vector. By default, it is :math:`2.38 / \sqrt{2\,\mathrm{ndim}}` as recommended by `ter Braak (2006) <http://www.stat.columbia.edu/~gelman/stuff_for_blog/cajo.pdf>`_.
+    aimh_prob : float, optional
+        probability to draw an adaptive independence Metropolis Hastings (AIMH) proposal. By default this is set to :math:`0.1`.
+    df_proposal_dist : float, optional
+        degrees of freedom of the multivariate t distribution used for AIMH proposals. Defaults to :math:`10`.
+    rho : float, optional
+        decay parameter for the aimh proposal mean and covariances. Defaults to :math:`0.999`.
     """
 
     def __init__(
-        self,
-        sigma=1.0e-5,
-        gamma=None,
-        aimh_prob=0.1,
-        df_proposal_dist=10,
-        **kwargs
+        self, sigma=1.0e-5, gamma=None, aimh_prob=0.1, df_proposal_dist=10, rho=.999, **kwargs
     ):
         if multivariate_t is None:
             raise ImportError(
@@ -59,10 +61,10 @@ class DIMEMove(RedBlueMove):
 
         self.sigma = sigma
         self.g0 = gamma
+        self.decay = rho
         self.aimh_prob = aimh_prob
         self.dft = df_proposal_dist
 
-        kwargs["nsplits"] = 1
         super(DIMEMove, self).__init__(**kwargs)
 
     def setup(self, coords):
@@ -80,9 +82,13 @@ class DIMEMove(RedBlueMove):
             self.prop_mean = np.zeros(npar)
             self.accepted = np.ones(nchain, dtype=bool)
             self.cumlweight = -np.inf
+        else:
+            # update AIMH proposal distribution
+            self.update_proposal_dist(coords)
 
     def propose(self, model, state):
-        """Wrap original propose to get the some info on the current state"""
+        """Wrap original propose to get the some info on the current state
+        """
 
         self.lprobs = state.log_prob
         state, accepted = super(DIMEMove, self).propose(model, state)
@@ -90,16 +96,17 @@ class DIMEMove(RedBlueMove):
         return state, accepted
 
     def update_proposal_dist(self, x):
-        """Update proposal distribution with ensemble `x`"""
+        """Update proposal distribution with ensemble `x`
+        """
 
         nchain, npar = x.shape
 
         # log weight of current ensemble
-        lweight = (
-            logsumexp(self.lprobs)
-            + np.log(sum(self.accepted))
-            - np.log(nchain)
-        )
+        if self.accepted.any():
+            lweight = logsumexp(self.lprobs) + \
+                np.log(sum(self.accepted)) - np.log(nchain)
+        else:
+            lweight = -np.inf
 
         # calculate stats for current ensemble
         ncov = np.cov(x.T, ddof=1)
@@ -115,15 +122,16 @@ class DIMEMove(RedBlueMove):
             np.exp(self.cumlweight - newcumlweight) * self.prop_mean
             + np.exp(lweight - newcumlweight) * nmean
         )
+        # self.cumlweight = newcumlweight + np.log(self.decay)
         self.cumlweight = newcumlweight
 
-    def get_proposal(self, x, dummy, random):
-        """Actual proposal function"""
+    def get_proposal(self, x, xref, random):
+        """Actual proposal function
+        """
 
+        xref = xref[0]
         nchain, npar = x.shape
-
-        # update AIMH proposal distribution
-        self.update_proposal_dist(x)
+        nref, _ = xref.shape
 
         # differential evolution: draw the indices of the complementary chains
         i0 = np.arange(nchain) + random.randint(1, nchain, size=nchain)
@@ -131,7 +139,7 @@ class DIMEMove(RedBlueMove):
         i1 += i1 >= i0
         # add small noise and calculate proposal
         f = self.sigma * random.randn(nchain)
-        q = x + self.g0 * (x[i0 % nchain] - x[i1 % nchain]) + f[:, np.newaxis]
+        q = x + self.g0 * (xref[i0 % nref] - xref[i1 % nref]) + f[:, None]
         factors = np.zeros(nchain, dtype=np.float64)
 
         # draw chains for AIMH sampling
@@ -145,7 +153,7 @@ class DIMEMove(RedBlueMove):
             size=sum(xchnge),
             random=random,
         )
-        lpropdold, lpropdnew = multivariate_t.logpdf(
+        lprop_old, lprop_new = multivariate_t.logpdf(
             np.vstack((x[None, xchnge], xcand[None])),
             self.prop_mean,
             self.prop_cov * (self.dft - 2) / self.dft,
@@ -154,6 +162,6 @@ class DIMEMove(RedBlueMove):
 
         # update proposals and factors
         q[xchnge, :] = np.reshape(xcand, (-1, npar))
-        factors[xchnge] = lpropdold - lpropdnew
+        factors[xchnge] = lprop_old - lprop_new
 
         return q, factors
